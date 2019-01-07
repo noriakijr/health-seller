254338cd8c3c1be6d0973f27c4438813e5e44e6b
Noriaki Odan Junior <noriaki.odan@gmail.com>
2019-01-07 00:21:49 -0200

    Configure spring boot

--- src/main/kotlin/com/noriakijr/healthseller/Application.kt
+++ src/main/kotlin/com/noriakijr/healthseller/Application.kt
@@ -1,5 +1,17 @@
 package com.noriakijr.healthseller
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println("Hello World")
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!!!")
+    runApplication<Application>(*args)
 }
